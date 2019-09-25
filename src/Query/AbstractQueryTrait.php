483f82f986d11bfb8bc900b7e76c5273455ee665
AllenJB <github@allenjb.me.uk>
2019-09-25 11:16:18 +0100

    Fix bulk-inserts (addRows) returning an empty bind values array

--- src/Query/AbstractQueryTrait.php
+++ src/Query/AbstractQueryTrait.php
@@ -19,24 +19,35 @@ trait AbstractQueryTrait
     {
         $retVal = [];
         foreach ($this->bind_values as $name => $value) {
-            if (is_object($value) && (($value instanceof \DateTimeImmutable) || ($value instanceof \DateTime))) {
-                $clone = clone $value;
-                if (ExtendedPdo::getSetTimeZoneUTC()) {
-                    $tz = new \DateTimeZone("UTC");
-                    // This reassignment handles DateTimeImmutable instances, which never modify themselves but return a new instance
-                    $clone = $clone->setTimezone($tz);
-                }
-                $value = $clone->format('Y-m-d H:i:s');
-            }
-            if (is_bool($value)) {
-                $value = ($value ? 1 : 0);
-            }
+            $retVal[$name] = $this->convertBindValue($value);
+        }
 
-
-            $retVal[$name] = $value;
+        if (isset($this->bind_values_bulk)) {
+            foreach ($this->bind_values_bulk as $name => $value) {
+                $retVal[$name] = $this->convertBindValue($value);
+            }
         }
 
         return $retVal;
+    }
+
+
+    protected function convertBindValue($value)
+    {
+        if (is_object($value) && (($value instanceof \DateTimeImmutable) || ($value instanceof \DateTime))) {
+            $clone = clone $value;
+            if (ExtendedPdo::getSetTimeZoneUTC()) {
+                $tz = new \DateTimeZone("UTC");
+                // This reassignment handles DateTimeImmutable instances, which never modify themselves but return a new instance
+                $clone = $clone->setTimezone($tz);
+            }
+            $value = $clone->format('Y-m-d H:i:s');
+        }
+        if (is_bool($value)) {
+            $value = ($value ? 1 : 0);
+        }
+
+        return $value;
     }
 
 }
