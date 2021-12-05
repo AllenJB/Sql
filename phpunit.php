ad6a6f7e775b271ebcd15e0f202b3e54210d9b84
AllenJB <github@allenjb.me.uk>
2021-12-05 10:02:01 +0000

    Add unit tests covering existing dependency tests

---
+++ phpunit.php
@@ -0,0 +1,4 @@
+<?php
+$autoloader = require(__DIR__ ."/vendor/autoload.php");
+$autoloader->addPsr4("Aura\\SqlQuery\\", __DIR__ ."/vendor/aura/sqlquery/tests/");
+
---
+++ tests/Query/Mysql/UpdateTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types = 1);
+
+namespace AllenJB\SqlQuery\Mysql;
+
+use AllenJB\SqlQuery\QueryFactory;
+
+class UpdateTest extends \Aura\SqlQuery\Mysql\SelectTest
+{
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->query_factory = new QueryFactory($this->db_type);
+    }
+
+}
